96432c24e32cabd659572ac13aca21e38389dee7
René Mäkeler <maekeler@marmalade.de>
2021-04-22 21:18:47 +0200

    Restrict rendering of DesktopNavigation to 6 items

--- patterns/core/GlobalNavigation/DesktopNavigation/index.js
+++ patterns/core/GlobalNavigation/DesktopNavigation/index.js
@@ -3,10 +3,12 @@ import FirstLevelNavigationItem from './FirstLevelNavigationItem'
 export default function DesktopNavigation(props) {
   const { menu = [] } = props
 
+  const rendered = menu.slice(0, 6)
+
   return (
     <nav className="desktop-navigation__wrapper">
       <ul className="desktop-navigation">
-        {menu.map((entry) => (
+        {rendered.map((entry) => (
           <FirstLevelNavigationItem key={entry.id} {...entry} />
         ))}
       </ul>
