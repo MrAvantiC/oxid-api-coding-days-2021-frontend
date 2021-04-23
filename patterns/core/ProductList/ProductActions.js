101d9b240a47ac2945c6628081f08f23b59d0572
René Mäkeler <maekeler@marmalade.de>
2021-04-23 12:21:45 +0200

    Change wording of button in ProductTile

--- patterns/core/ProductList/ProductActions.js
+++ patterns/core/ProductList/ProductActions.js
@@ -1,12 +1,9 @@
 import { Button } from '../..'
-import { useTranslation } from '../../../utils'
 
 export default function ProductActions() {
-  const { t } = useTranslation()
-
   return (
     <div className="product-item__actions">
-      <Button variant="primary">{t('PRODUCT_TILE_ADD_TO_CART')}</Button>
+      <Button variant="primary">Zum Produkt</Button>
     </div>
   )
 }
